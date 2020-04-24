ad32863569b87fc5f2e59d7d0b9459b1a97fb397
John Uthus <john.a.uthus.jr1@gmail.com>
2020-04-24 15:39:21 -0700

    added dob validation

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ function main() {
     resetErrorMessages();
     isTextPresent("first-name", "First name is required");
     isTextPresent("last-name", "Last name is required");
+    let dobBox = document.getElementById("dob");
+    let dob = dobBox.value;
+    if (!isValidDate(dob)) {
+        dobBox.nextElementSibling.innerHTML = "Invalid date foramt mm/dd/yyyy or m/d/yyyy";
+    }
+}
+function isValidDate(input) {
+    let pattern = /\d{1,2}\/\d{1,2}\/\d{4}/g;
+    return pattern.test(input);
 }
 function resetErrorMessages() {
     let allSpans = document.querySelectorAll("form span");
--- ts/main.ts
+++ ts/main.ts
@@ -7,6 +7,18 @@ function main():void{
     resetErrorMessages();
     isTextPresent("first-name", "First name is required");
     isTextPresent("last-name", "Last name is required");
+
+    let dobBox = <HTMLInputElement>document.getElementById("dob");
+    let dob = dobBox.value;
+    if(!isValidDate(dob)){
+        dobBox.nextElementSibling.innerHTML = "Invalid date foramt mm/dd/yyyy or m/d/yyyy";
+    }
+}
+
+function isValidDate(input:string):boolean{
+    // Validating mm/dd/yyyy and m/d/yyyy
+    let pattern = /\d{1,2}\/\d{1,2}\/\d{4}/g;
+    return pattern.test(input);
 }
 
 /**
